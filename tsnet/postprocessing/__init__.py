aa95112138cf02b7d3cd1c7e117a59ce914a43df
rkmazumder <rkmazumder@gmail.com>
2020-02-19 12:16:34 -0500

    Update __init__.py

--- tsnet/postprocessing/__init__.py
+++ tsnet/postprocessing/__init__.py
@@ -3,5 +3,3 @@ The tsnet.postprocessing package contains functions to
 postprocess the simulation results. 
 
 """
-from .time_history import plot_head_history, plot_velocity_history
-from .detect_cusum import detect_cusum
