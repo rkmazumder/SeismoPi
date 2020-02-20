b9ed374305b3d31b2144ec92549181bfc6dbf99c
rkmazumder <rkmazumder@gmail.com>
2020-02-20 14:58:13 -0500

    Update topology.py

--- tsnet/network/topology.py
+++ tsnet/network/topology.py
@@ -1,122 +1,54 @@
 """
-The tsnet.network.topology figure out the topology, i.e.,
-upstream and downstream adjacent links for each pipe, and
-store the information in lists.
-
+This file to estimate distances between various points
 """
 
-def topology(wn):
-    """Figure out the topology of the network
+import numpy as np
+import pandas as pd
+from scipy.spatial import distance
+import matplotlib.pyplot as plt
+import networkx as nx
 
-    Parameters
-    ----------
-    wn : wntr.network.model.WaterNetworkModel
-        .inp file used for EPAnet simulation
-    npipe : integer
-        Number of pipes
+class Distance:
+    # Constructor
+    def __init__(self, ex, ey, M):
+        self.ex = ex  # Create an instance variable
+        self.ey = ey
+        self.M=Mag
+    def com_pga_dist(data,ex,ey,Mag):
+        r = []
+        PGA = []
+        PGV = []
+        pos = {}
+        for index, row in data.iterrows():
+            x = row['x']
+            y = row['y']
+            dist = distance.euclidean((ex,ey), (x,y))/1000
+            P = (403.8*np.power(10, 0.265*Mag)*np.power(dist+30, -1.218))/981
+            V = (np.power(10, -0.848 + 0.775*Mag + -1.834*np.log10(dist+17)))/100
+            r.append(dist)
+            PGA.append(P)
+            PGV.append(V)
+            pos[int(row['id'])]=(x,y)
+        r = np.array(r)
+        PGA = np.array(PGA)
+        PGV = np.array(PGV)
+        return r, PGA, PGV, pos
 
-    Returns
-    -------
-    links1 : list
-        The id of adjacent pipe on the start node.
-        The sign represents the direction of the pipe.
-        + : flowing into the junction
-        - : flowing out from the junction
-    links2 : list
-        The id of adjacent pipe on the end node.
-        The sign represents the direction of the pipe.
-        + : flowing into the junction
-        - : flowing out from the junction
-    utype : list
-        The type of the upstream adjacent links.
-        If the link is not pipe, the name of that link
-        will also be included.
-        If there is no upstream link, the type of the start node
-        will be recorded.
-    dtype : list
-        The type of the downstream adjacent links.
-        If the link is not pipe, the name of that link
-        will also be included.
-        If there is no downstream link, the type of the end node
-        will be recorded.
-    """
-    G = wn.get_graph()
-    npipe = wn.num_pipes
-    length = wn.query_link_attribute('length')
-    G.weight_graph(link_attribute = length)
-
-    # add 'id' attribute to networkx links
-    i =1
-    for ln, link in wn.links():
-        G.edges[link.start_node_name,link.end_node_name, ln]['id'] = i
-        i+=1
-
-    # allocate the parameters
-    links1 = [0] * len(wn.links)
-    links2 = [0] * len(wn.links)
-    utype = [('Pipe',0)] * npipe
-    dtype = [('Pipe',0)] * npipe
-
-    # Adjcant pipes for each pipe IN:+; OUT:-
-    for _, link in wn.links():
-        pn = link.id
-        links1[int(pn)-1] = [int(p['id'])
-                           for _, attr in G.pred[link.start_node_name].items()
-                           for _,p in attr.items()
-                           if p['id'] != pn]
-
-        for _, attr in G.succ[link.start_node_name].items() :
-            for _,p in attr.items():
-                if p['id'] != pn:
-                    links1[int(pn)-1].append(-1* int(p['id']))
-
-
-        # right (end) adjcant pipes
-        links2[int(pn)-1] = [int(p['id'])
-                       for _, attr in G.pred[link.end_node_name].items()
-                       for _,p in attr.items()
-                       if p['id'] != pn]
-
-        for _, attr in G.succ[link.end_node_name].items():
-            for _,p in attr.items():
-                if p['id'] != pn:
-                    links2[int(pn)-1].append(-1*int(p['id']))
-
-    #figure out downstream type and upstream type
-    for _,pipe in wn.pipes():
-        pn = pipe.id-1
-        if links1[pn] :
-            if max(map(abs, links1[pn])) > npipe:
-                utype[pn] = [(l.link_type,l.name)
-                         for _,l in wn.links()
-                         if l.id == abs(links1[pn][0])][0]
-
-                if links1[abs(links1[pn][0])-1] and links2[abs(links1[pn][0])-1]:
-                    links1[pn] = [i
-                    for i in [links1[abs(links1[pn][0])-1], links2[abs(links1[pn][0])-1]]
-                    if abs(i[0]) -1 != pn][0]
-                else:
-                    links1[pn] = ['End']
-
-        else:
-            utype[pn] = (wn.nodes[pipe.start_node_name].node_type,
-                         wn.nodes[pipe.start_node_name])
-
-        if links2[pn] :
-            if max(map(abs, links2[pn])) > npipe:
-                dtype[pn] = [(l.link_type,l.name)
-                         for _,l in wn.links()
-                         if l.id == abs(links2[pn][0])][0]
-
-                if links1[abs(links2[pn][0])-1] and links2[abs(links2[pn][0])-1]:
-                    links2[pn] = [i
-                    for i in [links1[abs(links2[pn][0])-1], links2[abs(links2[pn][0])-1]]
-                    if abs(i[0]) -1 != pn][0]
-                else:
-                    links2[pn] = ['End']
-
-        else:
-            dtype[pn] = (wn.nodes[pipe.end_node_name].node_type,
-                         wn.nodes[pipe.end_node_name])
-
-    return links1, links2, utype, dtype
+    def pga_for_link(link,node,ex,ey,Mag):
+        r = []
+        PGA = []
+        PGV = []
+        for index, row in link.iterrows():
+            start_node = row['start_node']
+            end_node = row['end_node']
+            start_x, start_y = node.loc[node['id']==start_node, ['x','y']].values[0]
+            end_x, end_y = node.loc[node['id']==end_node, ['x','y']].values[0]
+            dist_start = distance.euclidean((ex,ey), (start_x,start_y))/1000
+            P_start = (403.8*np.power(10, 0.265*Mag)*np.power(dist_start+30, -1.218))/980
+            V_start = (np.power(10, -0.848 + 0.775*Mag + -1.834*np.log10(dist_start+17)))/100
+            dist_end = distance.euclidean((ex,ey), (end_x,end_y))/1000
+            P_end = 403.8*np.power(10, 0.265*Mag)*np.power(dist_end+30, -1.218)/980
+            V_end = (np.power(10, -0.848 + 0.775*Mag + -1.834*np.log10(dist_end+17)))/100
+            PGA.append((P_start+P_end)/2)
+            PGV.append((V_start+V_end)/2)
+        return PGA, PGV
